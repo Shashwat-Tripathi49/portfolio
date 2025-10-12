6544083c9d3d2fc1fe7930881fbb7190527278fd
Shashwat-Tripathi49 <shashwatt49@gmail.com>
2025-10-13 03:40:30 +0530

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
   description: "Portfolio of Shashwat Tripathi projects, skills, and experience in web development and design.",
 };
 
+// navneet
+
 export default function RootLayout({
   children,
 }: Readonly<{
